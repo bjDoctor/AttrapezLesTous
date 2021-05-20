3aaedc8f84425d751c2464efd5ed5c5bfc1da9f9
Bast <bastien.jordi@gmail.com>
2021-05-20 19:14:52 +0100

    Added Domain F# class library: defining types

---
+++ src/Domain/Types.fs
@@ -0,0 +1,12 @@
+﻿//////
+/// Module defining the types used throughout the application
+//////
+
+module Domain.Types
+
+type Pokemon = {
+    Name: string
+    Desctiption: string
+    Habitat: string
+    IsLegendary: bool
+}
