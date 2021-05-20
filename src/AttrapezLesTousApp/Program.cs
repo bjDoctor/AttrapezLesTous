6c0387207c29976c421ded938cb8d21e00cc2123
Bast <bastien.jordi@gmail.com>
2021-05-20 18:53:37 +0100

    Added logging with Serilog

--- src/AttrapezLesTousApp/Program.cs
+++ src/AttrapezLesTousApp/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
 
 namespace AttrapezLesTousApp
 {
@@ -13,11 +14,36 @@ namespace AttrapezLesTousApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // The initial "bootstrap" logger is able to log errors during start-up. It's completely replaced by the
+            // logger configured in `UseSerilog()` below, once configuration and dependency-injection have both been
+            // set up successfully.
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+
+            Log.Information("Starting up!");
+
+            try
+            {
+                Log.Information("Starting application");
+                CreateHostBuilder(args).Build().Run();
+                Log.Information("Application stopped gracefully");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
